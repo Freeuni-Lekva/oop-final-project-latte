80875ad2d8d5c172f42612e88e5725dc103f6260
luka <lukakharbedia6@gmail.com>
2025-07-08 14:04:56 +0400

    created Quizzes and Questions tables

--- SQL/mysql.sql
+++ SQL/mysql.sql
@@ -9,3 +9,25 @@ CREATE TABLE Users (
     hash VARCHAR(64) NOT NULL,
     join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE Quizzes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    creator_id INT NOT NULL,
+    is_random_order BOOLEAN DEFAULT FALSE,
+    is_one_page BOOLEAN DEFAULT TRUE,
+    is_immediate_correction BOOLEAN DEFAULT FALSE,
+    is_practice BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (creator_id) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_id INT NOT NULL,
+    type ENUM('question_response', 'fill_in_the_blank', 'multiple_choice', 'picture_response_questions') NOT NULL,
+    question TEXT NOT NULL,
+    position INT NOT NULL,
+    FOREIGN KEY (quiz_id) REFERENCES Quizzes(id) ON DELETE CASCADE
+);
